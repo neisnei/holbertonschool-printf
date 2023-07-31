6f5205836a19866b5da1f73d68a5422498030c33
Natzu83 <natzuac@yahoo.com>
2023-07-31 19:14:17 +0000

    Decimal

--- functions.c
+++ functions.c
@@ -20,8 +20,12 @@ int _printString(va_list values)
 
 int _printdec(va_list values)
 {
-	(void) values;
-	return (0);
+	int num = va_arg(values, int);
+	int count = 0;
+
+	count += _write_dec(num);
+
+	return count;
 }
 
 int _printint(va_list ar_numlist)
