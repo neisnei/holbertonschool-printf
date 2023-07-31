45bebe86bef2711c595acd4677920a12c8b853c3
Natzu83 <natzuac@yahoo.com>
2023-07-31 19:39:02 +0000

    functions

--- functions.c
+++ functions.c
@@ -26,45 +26,12 @@ int _printString(va_list values)
 		count++;
 		str++;
 	}
+	return count;
 }
 
 int _printdec(va_list values)
 {
-	int num = va_arg(values, int);
-	int count = 0;
-
-	if (num < 0)
-	{
-		_putchar_c('-');
-		num *= -1;
-	}
-
-	if (num == 0)
-	{
-		_putchar_c('0');
-		return 1;
-	}
-
-	int digits = 0;
-	int temp = num;
-
-	while (temp > 0)
-	{
-		temp /= 10;
-		digits++;
-	}
-
-	while (digits > 0)
-	{
-		temp = num;
-		for (int i = 1; i < digits; i++)
-			temp /= 10;
-		temp %= 10;
-		_putchar_c('0' + temp);
-		digits--;
-		count++;
-	}
-}
+} 
 
 int _printint(va_list ar_numlist)
 {
