733968340697f042fd2ee5c4210a9af1ac037cb3
neisnei <130294790+neisnei@users.noreply.github.com>
2023-07-31 08:33:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,3 +7,33 @@ int main (void)
 	_printf("%c", 'N');
 	return (0);
 }
+
+int main (void)
+{
+	printf("%i", 'i');
+	return (0);
+}
+
+int main (void)
+{
+	printf("%d", 'd');
+	return (0);
+}
+
+int main (void)
+{
+	printf("%s", 's');
+	return (0);
+}
+
+int main (void)
+{
+	printf("%c", 'c');
+	return (0);
+}
+
+int main (void)
+{
+	printf("%%", '%');
+	return (0);
+}
