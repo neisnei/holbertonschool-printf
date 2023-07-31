0a147766915e9eecb1d3c09408a21167767b2f34
Natzu83 <natzuac@yahoo.com>
2023-07-31 18:53:11 +0000

    Fun

--- functions.c
+++ functions.c
@@ -14,15 +14,8 @@ int _printchar(va_list values)
 
 int _printString(va_list values)
 {
-	char *str = va_arg(values, char *);
-	int count = 0;
-
-	if (str == NULL)
-		str = "(null)";
-
-	count += _write_str(str);
-
-	return count;
+	(void) values;
+	return (0);
 }
 
 int _printdec(va_list values)
