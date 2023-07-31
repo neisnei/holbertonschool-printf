5200378aec63333795876061ad2f3537ca5741a2
neisnei <neischalyruidiaz@gmail.com>
2023-07-31 18:00:41 +0000

    README.md

--- main.c
+++ main.c
@@ -9,31 +9,31 @@ int main (void)
 	return (0);
 }
 
-int main (void)
+int _printint(va_list ar_numlist)
 {
 	printf("%i", 'i');
 	return (0);
 }
 
-int main (void)
+int _printdec(va_list values)
 {
 	printf("%d", 'd');
 	return (0);
 }
 
-int main (void)
+int _printString(va_list values);
 {
 	printf("%s", 's');
 	return (0);
 }
 
-int main (void)
+int _printchar(va_list values);
 {
 	printf("%c", 'c');
 	return (0);
 }
 
-int main (void)
+int _printf(const char *format, ...)
 {
 	printf("%%", '%');
 	return (0);
